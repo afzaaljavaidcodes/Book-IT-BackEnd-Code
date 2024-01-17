85baba6cd46089310eb80365a10ee9c9dfce69b8
afzaaljavaidcoderepo <64521405+afzaaljavaid47@users.noreply.github.com>
2024-01-17 16:55:58 +0500

    code deploy

---
+++ models/Book.js
@@ -0,0 +1,33 @@
+const mongoose=require('mongoose');
+
+const BookSchema=new mongoose.Schema({
+    BookISBN:{
+        type:Number,
+        required:true
+    },
+    BookTitle:{
+        type:String,
+        required:true,
+    },
+    BookAuthor:{
+        type:String,
+        required:true,
+    },
+    BookGenre:{
+        type:String,
+        required:true
+    },
+    BookSummary:{
+        type:String,
+        required:true
+    },
+    BookLink:{
+        type:String
+    },
+    UserName:{
+        type:String,
+        required:true
+    }
+})
+const book=mongoose.model("Book",BookSchema);
+module.exports=book;
